2d9a4731c180568c9ff677370442c807f84164b3
Abhishekayodhyaaaa <shivmangal857418@gmail.com>
2025-03-05 01:40:45 +0530

    Update Master.py

--- Master.py
+++ Master.py
@@ -323,7 +323,7 @@ async def remove_user(update: Update, context: CallbackContext):
         await context.bot.send_message(chat_id=chat_id, text=f"*⚠️ User {target_user_id} ye chutiya is bot m nhi h malik.*", parse_mode='Markdown')
 
 def main():
-    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    application = Updater().token(TELEGRAM_BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("MasterBhaiyaa", MasterBhaiyaa))
     application.add_handler(CommandHandler("attack", attack))
