2c17db335026b649ea4054be27924a35f3f546fb
Abhishekayodhyaaaa <shivmangal857418@gmail.com>
2025-03-09 09:39:47 +0530

    Update Master.py

--- Master.py
+++ Master.py
@@ -172,7 +172,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./MasterBhaiyaa {ip} {port} {duration} 300 "
+        command = f"./MasterBhaiyaa {ip} {port} {duration}"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
